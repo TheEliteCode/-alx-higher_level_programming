80b08d278d6ed45befb5f5bfd0e6191ae58e4a83
TheEliteCode <davode12@gmail.com>
2023-01-05 16:05:33 -0800

    corrected task one

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-# 1-calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
     """Print the sum, difference, multiple and quotient of 10 and 5."""
@@ -13,4 +11,3 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
