916ec1c649e97ba72eae776487458fa0bcb1529c
David Alexander <davode12@gmail.com>
2023-02-12 17:22:52 +0100

    updated task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,19 +1,15 @@
-#!user/bin/python3
+#!/usr/bin/python3
 # 3-write_file.py
-# davode12@gmail.com(TheEliteCode)
-
-"""Defines a file-writing function"""
+"""Defines a file-writing function."""
 
 
 def write_file(filename="", text=""):
     """Write a string to a UTF8 text file.
-    
     Args:
         filename (str): The name of the file to write.
         text (str): The text to write to the file.
     Returns:
         The number of characters written.
     """
-
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
