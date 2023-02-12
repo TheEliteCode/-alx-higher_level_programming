9052f3052f378b3c57f61603f5c60c7055fb9e66
TheEliteCode <davode12@gmail.com>
2023-02-12 18:12:20 +0100

    completed

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-# 3-to_json_string.py
-# davode12@gmail.com    github= TheEliteCode
-
+# 5-to_json_string.py
+# Brennan D Baraban <375@holbertonschool.com>
+"""Defines a string-to-JSON function."""
 import json
 
 
 def to_json_string(my_obj):
-    """To return the JSON representation of a string object."""
+    """Return the JSON representation of a string object."""
     return json.dumps(my_obj)
