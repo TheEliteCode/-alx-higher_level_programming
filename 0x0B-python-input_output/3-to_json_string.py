17b34d01cfa22ebda06e954a603baaf5f4d02d7e
David Alexander <davode12@gmail.com>
2023-02-12 17:51:13 +0100

    A function that returns the json of a string

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 3-to_json_string.py
+# davode12@gmail.com    github= TheEliteCode
+
+import json
+
+
+def to_json_string(my_obj):
+    """To return the JSON representation of a string object."""
+    return json.dumps(my_obj)
